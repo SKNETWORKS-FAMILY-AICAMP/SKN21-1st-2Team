248fd81a9f5361a27170884f298fe43d98b0f51f
Woo Jaehyun <wjaehyun97@gmail.com>
2025-10-23 12:32:11 +0900

    file name set, need change code

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -6,8 +6,7 @@ Date: 2025-10-23
 Description: 폴더를 모듈로 인식하여 실행 코드 정의
 """
 
-
-# 모듈 단축 import
+# 모듈 단축 import 작성해야함
 from .db_connection import get_connection
 from .insert_data import insert_vehicle_data
 from .fetch_data import fetch_vehicle_data
