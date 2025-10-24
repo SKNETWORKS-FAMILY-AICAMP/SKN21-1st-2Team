7905a5b359e230a7de1bbd63fd2e5615c92cae75
Jung Deokkyu <duck213@gmail.com>
2025-10-24 10:40:21 +0900

    test

--- src/app.py
+++ src/app.py
@@ -1,16 +1,9 @@
 from database import db_connection
 from database import insert_data
-from database import fetch_data
 
 if __name__ == "__main__":
-    # insert_data.h2_FAQ("data\processed\h2_faq.csv")
+    insert_data.h2_FAQ("data\processed\h2_faq.csv")
     # insert_data.annual_H2_ev_registrations("data\processed\\annual_h2_ev_registrations.csv")
     # insert_data.h2_station_info("data\processed\h2_station_info.csv")
     # insert_data.h2_station_by_region("data\processed\h2_stations_by_region.csv")
     # insert_data.ev_station_by_region("data\processed\ev_stations_by_region.csv")
-
-    fetch_data.fetch_station_info()
-    fetch_data.fetch_vehicle_trend()
-    fetch_data.fetch_ev_stations_region()
-    fetch_data.fetch_station_count_by_region()
-    #fetch_data.fetch_faq()
