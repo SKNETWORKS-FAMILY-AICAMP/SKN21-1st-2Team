ffea9964cfbbd201323b37a1024617405eee4ce0
Woo Jaehyun <wjaehyun97@gmail.com>
2025-10-24 14:26:27 +0900

    test

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 from database import db_connection
 from database import insert_data
-from src.database import fetch_data
+# from src.database import fetch_data
 import database.fetch_data as fs
+from visualization import plot
 
 if __name__ == "__main__":
+    # db_connection.get_connection()
     # insert_data.h2_FAQ("data\processed\h2_faq.csv")
     # insert_data.annual_H2_ev_registrations("data\processed\\annual_h2_ev_registrations.csv")
     # insert_data.h2_station_info("data\processed\h2_station_info.csv")
@@ -11,10 +13,11 @@ if __name__ == "__main__":
     # insert_data.ev_station_by_region("data\processed\ev_stations_by_region.csv")
 
     
-    fetch_data.fetch_faq() # faq select test, 이후 주석처리해야함
+    # fetch_data.fetch_faq() # faq select test, 이후 주석처리해야함
     #fs.fetch_h2_station_info() # 수소차 충전소 조회
     #fs.fetch_h2_stations_by_region() #지역별 수소차 충전소 개수 조회
     #fs.fetch_annual_h2_ev_registrations() # 연도별 H2/EV 자동차 등록 현황 조회
-    fs.h2_stats()
+    # fs.h2_stats()
     #fs.fetch_ev_stations_region() # 지역별 전기차 충전소 개수
-    #fs.fetch_faq()
+    # fs.fetch_faq()
+    plot.abc()
