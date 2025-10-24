98abc92700a862cba86faf43c5d72e539b0a9310
Woo Jaehyun <wjaehyun97@gmail.com>
2025-10-24 15:39:32 +0900

    푸쉬

--- src/app.py
+++ src/app.py
@@ -1,24 +1,18 @@
 from database import db_connection
-from database import insert_data
-# from src.database import fetch_data
-import database.fetch_data as fs
-from visualization import plot
-
-import database.fetch_data as fs
-
-from visualization import plot
+# from database import insert_data
+from database import fetch_data
+# from visualization import plot
 
 if __name__ == "__main__":
 
-
-    plot.plot()
+    # plot.plot()
 
     # db_connection.get_connection()
     #insert_data.h2_faq("./../data/processed/h2_faq_clean.csv")
     #insert_data.annual_H2_ev_registrations("./../data/processed/annual_h2_ev_registrations.csv")
     #insert_data.h2_station_info("./../data/processed/h2_station_info_map.csv")
     #insert_data.h2_stations_by_region("./../data/processed/h2_stations_by_region_map.csv")
-    insert_data.ev_stations_by_region("./../data/processed/ev_stations_by_region_map.csv")
+    # insert_data.ev_stations_by_region("./../data/processed/ev_stations_by_region_map.csv")
     #insert_data.region("./../data/processed/region.csv")
 
     
@@ -28,5 +22,4 @@ if __name__ == "__main__":
     #fs.fetch_annual_h2_ev_registrations() # 연도별 H2/EV 자동차 등록 현황 조회
     # fs.h2_stats()
     #fs.fetch_ev_stations_region() # 지역별 전기차 충전소 개수
-    # fs.fetch_faq()
->>>>>>> 67b2648d732b55e2de8492a8d30ec1f13ffbe47d
+    fetch_data.fetch_faq()
--- src/visualization/plot.py
+++ src/visualization/plot.py
@@ -1,61 +1,50 @@
 import pandas as pd
-
+import io
 
 import matplotlib.pyplot as plt
 from matplotlib import font_manager
 from matplotlib.ticker import ScalarFormatter
-import io
-from database import fetch_data
 
 from database import fetch_data
 
-<<<<<<< HEAD
+def plot():
+    df = fetch_data.fetch_annual_h2_ev_registrations()  # DB에서 데이터 불러오기
+    print(df)
+    font_path = '/System/Library/Fonts/Supplemental/AppleGothic.ttf'
+    font_prop = font_manager.FontProperties(fname=font_path)
+    plt.rcParams['font.family'] = font_prop.get_name()
+    plt.rcParams['axes.unicode_minus'] = False
 
-# def abc():
-#     print(fetch_data.fetch_vehicle_trend())
-#     print('123')
+    #scale 설정
+    formatter = ScalarFormatter(useMathText=False)
+    formatter.set_scientific(False)
 
+    #수소차 연도별 
+    plt.figure(figsize=(10, 5))
+    plt.plot(df["year"], df["h2_car_total"], marker="o", label="수소차 개수", color="blue")
+    plt.title("연도별 수소차 개수", fontsize=16)
+    plt.xlabel("연도")
+    plt.ylabel("차량 대수")
+    plt.grid(True)
+    plt.legend()
+    plt.gca().yaxis.set_major_formatter(formatter)
+    plt.show()
 
-# def plot():
-#     df = fetch_data.fetch_vehicle_trend()  # DB에서 데이터 불러오기
-
-
-#     font_path = '/System/Library/Fonts/Supplemental/AppleGothic.ttf'
-#     font_prop = font_manager.FontProperties(fname=font_path)
-#     plt.rcParams['font.family'] = font_prop.get_name()
-#     plt.rcParams['axes.unicode_minus'] = False
-
-#     #scale 설정
-#     formatter = ScalarFormatter(useMathText=False)
-#     formatter.set_scientific(False)
-
-#     #수소차 연도별 
-#     plt.figure(figsize=(10, 5))
-#     plt.plot(df["year"], df["h2_car_total"], marker="o", label="수소차 개수", color="blue")
-#     plt.title("연도별 수소차 개수", fontsize=16)
-#     plt.xlabel("연도")
-#     plt.ylabel("차량 대수")
-#     plt.grid(True)
-#     plt.legend()
-#     plt.gca().yaxis.set_major_formatter(formatter)
-#     plt.show()
-
-#     #전기차 연도별
-#     plt.figure(figsize=(10, 5))
-#     plt.plot(df["year"], df["ev_car_total"], marker="o", label="전기차 개수", color="green")
-#     plt.title("연도별 전기차 개수", fontsize=16)
-#     plt.xlabel("연도")
-#     plt.ylabel("차량 대수")
-#     plt.grid(True)
-#     plt.legend()
-#     plt.gca().yaxis.set_major_formatter(formatter)
-#     plt.show()
-
-#     # print("123")
+    #전기차 연도별
+    plt.figure(figsize=(10, 5))
+    plt.plot(df["year"], df["ev_car_total"], marker="o", label="전기차 개수", color="green")
+    plt.title("연도별 전기차 개수", fontsize=16)
+    plt.xlabel("연도")
+    plt.ylabel("차량 대수")
+    plt.grid(True)
+    plt.legend()
+    plt.gca().yaxis.set_major_formatter(formatter)
+    plt.show()
 
 
 
-df = fetch_data.fetch_vehicle_trend()
+
+df = fetch_data.fetch_annual_h2_ev_registrations()
 
 if df.empty:
     print("데이터가 없습니다. 그래프를 그릴 수 없습니다.")
@@ -91,9 +80,3 @@ else:
     plt.legend()
     plt.gca().yaxis.set_major_formatter(formatter)
     plt.show()
-=======
-# conn = get_connection()
-# df = get_vehicle_trend_data(conn)
-# fig = plot_vehicle_trend(df)
-# st.pyplot(fig)
->>>>>>> 67b2648d732b55e2de8492a8d30ec1f13ffbe47d
