df86897d5e2aa4ce7aab38d14f15c1ac0ee187bf
MJ <silentkit12@gmail.com>
2025-10-24 11:12:14 +0900

    filename

--- streamlitapp/pages/1_📈친환경차_연도별_증감그래프.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-#. 꺾은 선 그래프 두개. 비교
-#. x축 연도 18년도 ~ 25년도 / y축 증가율(10,000대 단위로 눈금 표시) 최저 0~ 100
-
-st.title("📊 연도별 친환경차 등록 그래프")
-st.subheader("2018년~2025년 사이의 전기차와 수소차의 비교")
-
