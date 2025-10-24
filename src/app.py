1b5334af092a84e13b0b33705da5defd1be4a260
Woo Jaehyun <wjaehyun97@gmail.com>
2025-10-24 10:12:05 +0900

    insert 기능 완료, db insert 완료

--- src/app.py
+++ src/app.py
@@ -2,9 +2,10 @@ from database import db_connection
 from database import insert_data
 
 if __name__ == "__main__":
-    insert_data.h2_FAQ("data\processed\h2_faq.csv")
-    # insert_data.annual_H2_ev_registrations("data\processed\\annual_h2_ev_registrations.csv")
-    # insert_data.h2_station_info("data\processed\h2_station_info.csv")
-    # insert_data.h2_station_by_region("data\processed\h2_stations_by_region.csv")
-    # insert_data.ev_station_by_region("data\processed\ev_stations_by_region.csv")
-    
+    # db에 csv 값 입력 완료, 재사용 기능x(주석 풀지 마세요)
+    # insert_data.h2_FAQ("data\processed\h2_faq.csv") # db 생성완료
+    # insert_data.annual_H2_ev_registrations("data\processed\\annual_h2_ev_registrations.csv") # db 생성 완료
+    # insert_data.h2_station_info("data\processed\h2_station_info.csv") # db 생성 완료
+    # insert_data.h2_stations_by_region("data\processed\h2_stations_by_region.csv") # db 생성 완료
+    # insert_data.ev_stations_by_region("data\processed\ev_stations_by_region.csv") # db 생성 완료
+    pass
